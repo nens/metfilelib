07040362ad1a5b2551860fc675f6daaf45e414e4
Heleen van de Schraaf <heleen.vandeschraaf@nelen-schuurmans.nl>
2015-07-01 12:13:11 +0200

    Constrain allowed characters in meting, profiel and reeks.
    Constrain the allowed characters between:
    
    * <METING> and </METING>
    * <REEKS> and </REEKS>
    * <PROFIEL> and <METING>

--- metfilelib/parser.py
+++ metfilelib/parser.py
@@ -78,7 +78,7 @@ def parse_series(file_object):
                 "Reeks regel moet beginnen met <REEKS> en eindigen met </REEKS>",
                 "MET_NOREEKS")
         else:
-            seriesre = re.compile("<REEKS>(.*),(.*),</REEKS>")
+            seriesre = re.compile("<REEKS>([\w.-]*),([\w.-]*),</REEKS>")
             match = seriesre.match(l)
 
             if match:
@@ -90,8 +90,9 @@ def parse_series(file_object):
                     "MET_REEKSELEMENTS")
 
                 # Try to continue parsing without the second comma
-                seriesre = re.compile("<REEKS>(.*),(.*)</REEKS>")
+                seriesre = re.compile("<REEKS>([\w.-]*),([\w.-]*)</REEKS>")
                 match = seriesre.match(l)
+
         file_object.next()
 
     profiles = []
@@ -121,7 +122,7 @@ def parse_profile(file_object):
             "Profiel regel moet beginnen met <PROFIEL>",
             "MET_NOPROFIEL")
 
-    profilere = re.compile("<PROFIEL>" + 10 * "([^,]*),")
+    profilere = re.compile("<PROFIEL>" + 10 * "([\w.-]*),")
     match = profilere.match(l)
 
     if not match:
@@ -220,10 +221,17 @@ def parse_meting(file_object):
         file_object.next()
         return
 
-    metingre = re.compile("<METING>(.*)</METING>")
+    metingre = re.compile("<METING>([\d.,-]*)</METING>")
     match = metingre.match(l)
 
-    groups = match.group(1).split(",")
+    try:
+        groups = match.group(1).split(",")
+    except AttributeError:
+        file_object.record_error(
+            "Een <METING> object bevat invalide tekens.",
+            "MET_METINGSIXVALUES")
+        file_object.next()
+        return
 
     if len(groups) == 7 and not groups[6]:
         # There are 7, but the last one is empty (extra comma):
